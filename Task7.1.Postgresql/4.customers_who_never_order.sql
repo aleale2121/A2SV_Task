686d18531f1bd4080fc67c552fe2134911e2300d
alefew <alefewyimer4@gmail.com>
2023-09-18 21:35:39 +0300

    sql problem solutions

---
+++ Task7.1.Postgresql/4.customers_who_never_order.sql
@@ -0,0 +1,9 @@
+SELECT
+    c.name as Customers
+FROM
+    Customers c
+WHERE
+    c.id NOT IN(
+       SELECT o.customerId FROM Orders o
+    );
+    
---
+++ Task7.1.Postgresql/5.employees_earning_morethan_manager.sql
@@ -0,0 +1,7 @@
+select e.name as Employee 
+from Employee e
+where e.salary > (
+    select m.salary 
+    from Employee m
+    where m.id = e.managerId
+);
